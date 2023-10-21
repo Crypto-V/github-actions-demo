539eb13ab179b1bd3ea361758608e424fdc2396b
Crypto-V <verejanvasile92@gmail.com>
2023-10-21 15:53:10 +0100

    changed java version 17->11

--- src/main/java/com/vincode/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/vincode/githubcicdactions/GithubCicdActionsApplication.java
@@ -18,12 +18,4 @@ public class GithubCicdActionsApplication {
         SpringApplication.run(GithubCicdActionsApplication.class, args);
     }
 
-//    echo "# github-actions-demo" >> README.md
-//    git init
-//    git add README.md
-//    git commit -m "first commit"
-//    git branch -M main
-//    git remote add origin https://github.com/Crypto-V/github-actions-demo.git
-//    git push -u origin main
-
 }
